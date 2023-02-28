8caed326cb2f96c3746c1cc292d24eb041ce0444
Joshua Rogers <jrogers@opera.com>
2023-02-28 19:10:47 +0100

    Remove _gtfobins and most of the files. Prepare for _gtfoargs

--- Makefile
+++ Makefile
@@ -10,5 +10,5 @@ bundle:
 	bundle install
 
 lint:
-	yamllint _gtfobins/*.md
+	yamllint _gtfoargs/*.md
 	scripts/validate-schema.py
--- scripts/validate-schema.py
+++ scripts/validate-schema.py
@@ -57,4 +57,4 @@ def validate_directory(root):
             sys.exit(1)
 
 if __name__ == '__main__':
-    validate_directory("_gtfobins/")
+    validate_directory("_gtfoargs/")
